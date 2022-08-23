727f4e3760e50d7dccddb88b867035bab8f8a4b7
Fabricio Tanjoni <fabriciotanjoni@gmail.com>
2022-08-23 20:40:01 -0300

    mdl_1_Exercício_02

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace md01_CadernoExercicios_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 2. Faça um algoritmo que:
+            a) Leia o nome;
+            b) Leia o sobrenome;
+            c) Concatene o nome com o sobrenome;
+            d) Apresente o nome completo.
+            */
+            string nome, sobrenome;
+
+            Console.WriteLine("\nDigite seu nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("\nDigite seu sobrenome:");
+            sobrenome = Console.ReadLine();
+
+            Console.WriteLine("'\nOlá " + nome + " " + sobrenome + "\nSeja Bem-vindo ao Sistema!");
+            
+        }
+    }
+}
